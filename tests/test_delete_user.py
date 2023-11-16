726a22fdcc4a100dc11e367359a32cee334ee706
eltonccdantas <elton.chagas.dantas@gmail.com>
2023-11-16 20:53:16 -0300

    rebasing issues

--- tests/test_delete_user.py
+++ tests/test_delete_user.py
@@ -4,11 +4,12 @@ from pages.ManagerCustomersPage import ManagerCustomersPage
 
 class Test4:
 
-    def test_delete_user(self, open_browser):
-        login_p = open_browser
+    def test_delete_user(self, create_user):
+        login_p, user = create_user
         login_p.click_bank_manager_btn()
-        customer_page = ManagerCustomersPage
 
-        customer_page.delete_user
+        customer_page = ManagerCustomersPage(login_p.driver)
+
+        customer_page.delete_user()
 
 
